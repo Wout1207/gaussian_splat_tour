179ff453778331c736ddf6faed6e283daadc9176
Vivian <viviantsang1310@gmail.com>
2025-05-21 19:48:36 +0200

    Created power supply and return button now disables the render objects

--- UnityGaussianSplatting/projects/GT_Tour/Assets/Scripts/Interaction.cs
+++ UnityGaussianSplatting/projects/GT_Tour/Assets/Scripts/Interaction.cs
@@ -8,28 +8,45 @@ public class DoorInteraction : MonoBehaviour
     public Camera cameraView;
     public float interactRange = 10f;
     public GameObject UI;
+    public GameObject[] renders;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cameraView.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit, interactRange))
-            {   
+            {
                 GameObject item = hit.collider.gameObject;
                 Debug.Log("Hit a :" + item.name);
-                
-                if(item.tag == "Item"){
+
+                if (item.tag == "Item")
+                {
                     UI.GetComponent<PopUpController>().showInfoScreen(item.GetComponent<Item>().itemData);
-                    item.GetComponent<Item>().render.SetActive(true);
+                    enableRender(item);
                 }
-                else if(item.tag == "Door"){
+                else if (item.tag == "Door")
+                {
                     item.GetComponent<Door>().renderRoom(this.transform);
-                }
                 }
             }
         }
+    }
+        
+    public void disableAllRenders() {
+        foreach (GameObject render in renders)
+        {
+            render.SetActive(false);
+        }        
+    }
+
+    public void enableRender(GameObject item) {
+        item.GetComponent<Item>().render.SetActive(true);
+    
+    }
     }   
+
+
